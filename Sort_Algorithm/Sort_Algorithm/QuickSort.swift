df8a2d885fbb7ee41caa828c6b0c6fe094f49e38
KyusokLee <kyusok1@naver.com>
2022-05-22 19:11:21 +0900

    Quick Sort Algorithm

--- Sort_Algorithm/Sort_Algorithm/QuickSort.swift
+++ Sort_Algorithm/Sort_Algorithm/QuickSort.swift
@@ -7,12 +7,14 @@
 
 import Foundation
 
+// 平均 O(nlogn) , 最悪: O(n ^ 2) 理由: pivotが最小値もしくは、最大値に設定された場合
+//  方法1: 最初の要素をpivotにした関数
 func quickSort(_ array: [Int]) -> [Int] {
     guard let first = array.first, array.count > 1 else {
         return array
     }
  
-    let pivot = first
+    let pivot = first // pivotをarrayの最初の要素に設定
     let left = array.filter { $0 < pivot }
     let right = array.filter { $0 > pivot }
     
@@ -21,7 +23,27 @@ func quickSort(_ array: [Int]) -> [Int] {
 }
 
 var pracArray = [1, 3, 8, 9, 12, 2, 7, 13, 2]
-print(quickSort(pracArray))
+print(quickSort(pracArray)) // [1, 2, 3, 7, 8, 9, 12, 13]
+var pracArray1_1 = [1, 3, 8, 9, 12, 2, 1, 7, 13, 2]
+print(quickSort(pracArray1_1)) // 重複の2は出力されない 出力: [1, 2, 3, 7, 8, 9, 12, 13]
 
-pracArray += [111]
-print(pracArray)
+//  方法2: 真ん中の要素をpivotにした関数, また、重複の要素も全部sortすることができる
+func quickSort2(_ array: [Int]) -> [Int] {
+    guard array.count > 1 else {
+        return array
+    }
+ 
+    let pivot = array[array.count / 2]
+    let left = array.filter { $0 < pivot }
+    let middle = array.filter { $0 == pivot }
+    let right = array.filter { $0 > pivot }
+    
+    // 配列を二つに分けて再帰を通して、目標のarrayを得る
+    return quickSort(left) + middle + quickSort(right) //[pivot]を真ん中に
+}
+
+var pracArray2 = [1, 3, 8, 9, 12, 2, 7, 13, 2]
+print(quickSort2(pracArray2)) // [1, 2, 3, 7, 8, 9, 12, 13]
+var pracArray3 = [1, 3, 8, 9, 12, 2, 1, 7, 13, 2]
+print(quickSort2(pracArray3)) // [1, 2, 2, 3, 7, 8, 9, 12, 13] 重複の要素 2 もちゃんとソートされた
+
--- Sort_Algorithm/Sort_Algorithm/main.swift
+++ Sort_Algorithm/Sort_Algorithm/main.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+//// arrayの確認練習
+//var pracArray = [1, 3, 8, 9, 12, 2, 7, 13, 2]
+//pracArray += [111]
+//print(pracArray) //[1, 3, 8, 9, 12, 2, 7, 13, 2, 111]

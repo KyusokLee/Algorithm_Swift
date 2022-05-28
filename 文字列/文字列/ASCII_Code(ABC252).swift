b5aad2ccb2fe59dae92e572acaae03752d2c36ec
KyusokLee <kyusok1@naver.com>
2022-05-28 16:49:38 +0900

    ASCII_Code(ABC252)_ 問題解決_ascii 変換

---
+++ 文字列/文字列/ASCII_Code(ABC252).swift
@@ -0,0 +1,15 @@
+//
+//  ASCII_Code.swift
+//  文字列
+//
+//  Created by Kyus'lee on 2022/05/28.
+//
+
+import Foundation
+//Atcoder Beginner Contest 252
+//A. ASCII Code
+
+let input = Int(readLine()!)!
+let a = Character(UnicodeScalar(input)!)
+// ただの UnicodeScalarだと、Scalar Typeになる
+print(a)
---
+++ 文字列/文字列/Takahashi's_failure(ABC252).swift
@@ -0,0 +1,8 @@
+//
+//  Takahashi's_failure(ABC252).swift
+//  文字列
+//
+//  Created by Kyus'lee on 2022/05/28.
+//
+
+import Foundation

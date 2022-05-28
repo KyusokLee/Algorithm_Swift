4612f04e986e25d62aff1f12d5416bb12b9b01d8
KyusokLee <kyusok1@naver.com>
2022-05-28 15:09:45 +0900

    miniMax_Sum_問題解決

---
+++ Math_problem/Math_problem/miniMax_Sum(HackerRank).swift
@@ -0,0 +1,28 @@
+//
+//  miniMax_Sum(HackerRank).swift
+//  Math_problem
+//
+//  Created by Kyus'lee on 2022/05/28.
+//
+
+import Foundation
+//HackerRank
+//mini-maxSum
+
+func mini_maxSum(arr: [Int]) -> Void {
+    var result = ""
+    let sortedArray = arr.sorted()
+    var dp = Array(repeating: 0, count: sortedArray.count)
+    dp[0] = sortedArray[0]
+    
+    for i in 1..<sortedArray.count {
+        dp[i] = dp[i - 1] + sortedArray[i]
+    }
+    dp[4] -= dp[0]
+    
+    result += "\(dp[3]) \(dp[4])"
+    print(result)
+}
+
+let inputArray = readLine()!.split(separator: " ").map { Int(String($0))! }
+mini_maxSum(arr: inputArray)

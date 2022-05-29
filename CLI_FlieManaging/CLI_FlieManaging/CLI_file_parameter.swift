da20de83a2a8480f2ab322879f47950adde0931f
KyusokLee <kyusok1@naver.com>
2022-05-29 23:39:52 +0900

    CLI_FileManaging_practice1

---
+++ CLI_FlieManaging/CLI_FlieManaging/CLI_file_parameter.swift
@@ -0,0 +1,8 @@
+//
+//  CLI_file_parameter.swift
+//  CLI_FlieManaging
+//
+//  Created by Kyus'lee on 2022/05/29.
+//
+
+import Foundation
---
+++ CLI_FlieManaging/CLI_FlieManaging/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  CLI_FlieManaging
+//
+//  Created by Kyus'lee on 2022/05/29.
+//
+
+import Foundation
+
+print("Hello, World!")
+

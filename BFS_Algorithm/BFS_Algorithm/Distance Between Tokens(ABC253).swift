b157eb5a34ceafb6e624780a75afbc5fa66b089e
KyusokLee <kyusok1@naver.com>
2022-06-02 20:43:33 +0900

    Distance Between Tokens_ABC253_問題解決_簡単なbfs

---
+++ BFS_Algorithm/BFS_Algorithm/Distance Between Tokens(ABC253).swift
@@ -0,0 +1,60 @@
+//
+//  File.swift
+//  BFS_Algorithm
+//
+
+import Foundation
+//Atcoder Beginners Contest 253
+//B. Distance Between Tokens
+// 🎖BFS
+typealias Locate = (row: Int, column: Int)
+let data = readLine()!.split(separator: " ").map { Int(String($0))! }
+let (rowSize, columnSize) = (data[0], data[1])
+let directionMove: [Locate] = [(0, 1), (0, -1), (1, 0), (-1, 0)] //東西南北
+var haveMoving = [Locate]()
+var map = [[Character]]()
+
+for i in 0..<rowSize {
+    let input = Array(readLine()!)
+    for j in 0..<columnSize {
+        if input[j] == "o" {
+            haveMoving.append((i, j))
+        }
+    }
+    map += [input]
+}
+
+print(bfs(haveMoving[0].row, haveMoving[0].column))
+
+func bfs(_ row: Int, _ column: Int) -> Int {
+    var neededVisitQueue = [(row, column, 0)]
+    var index = 0
+    var result = 0
+    var visited = Array(repeating: Array(repeating: false, count: columnSize), count: rowSize)
+    visited[row][column] = true
+
+    while index < neededVisitQueue.count {
+        let (curRow, curColumn, curCount) = neededVisitQueue[index]
+
+        if (curRow, curColumn) == haveMoving.last! {
+            result = curCount
+            break
+        }
+
+        for i in 0..<4 {
+            let (nextRow, nextColumn) = (curRow + directionMove[i].row, curColumn + directionMove[i].column)
+
+            if nextRow < 0 || nextRow >= rowSize || nextColumn < 0 || nextColumn >= columnSize || visited[nextRow][nextColumn] {
+                continue
+            }
+
+            visited[nextRow][nextColumn] = true
+            neededVisitQueue.append((nextRow, nextColumn, curCount + 1))
+        }
+
+        index += 1
+    }
+
+    return result
+}
+

00ce0c67f7d4e3c7db2b678734cd935545cf43c8
KyusokLee <kyusok1@naver.com>
2022-06-04 23:21:04 +0900

    Last Two Digits_ABC253_問題解決

---
+++ Math_problem/Math_problem/Last_Two_Digits(ABC254).swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Math_problem
+//
+//  Created by Kyus'lee on 2022/06/04.
+//
+
+import Foundation
+//Atcoder Beginners Contest 254
+//A. Last Two Digits
+// 🎖Math
+var data = readLine()!.map { String($0) }
+let result = "\(data[1])\(data[2])"
+print(result)

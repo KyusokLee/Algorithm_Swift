c331e01ee74f26b2f8a0401a3f05e02807821da0
KyusokLee <kyusok1@naver.com>
2022-06-11 18:23:55 +0900

    Term Project_9446_ 復習Series_DFS, Cycle Hard

--- DFS_Algorithm/DFS_Algorithm/main.swift
+++ DFS_Algorithm/DFS_Algorithm/main.swift
@@ -11,3 +11,21 @@ import Foundation
 //guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!) else {
 //    fatalError("Bad input")
 //}
+
+//// Array 活用練習
+//let rensyu = [3, 5, 7, 9, 11, 8]
+//var rensyu2 = [[Int]]()
+//
+//rensyu2 += [[2, 1]]
+//print(rensyu2)
+//rensyu2.append(Array(rensyu.firstIndex(of: 5)!...rensyu.endIndex - 1)) // ただ、1 2 3 4 5が格納される
+//print(rensyu2)
+//rensyu2 += [[3]]
+//print(rensyu2)
+//
+//let slicing = rensyu[1...rensyu.endIndex - 1] // [5, 7, 9, 11, 8]
+//print(rensyu[1...rensyu.endIndex - 1]) // [5, 7, 9, 11, 8]
+//print(type(of: slicing)) // ArraySlice<Int>型
+//let changeTypeToArray = Array(slicing)
+//print(changeTypeToArray)  // [5, 7, 9, 11, 8]
+//print(type(of: changeTypeToArray)) // Array<Int>型
